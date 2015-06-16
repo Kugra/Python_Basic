68e36b94da303e1f2af568939eb83baef534e9a7
Kugra <johan.kovalsikoski@gmail.com>
2015-06-16 14:27:41 -0300

    Detalhando funções uteis na biblioteca Math

---
+++ Math.py
@@ -0,0 +1,19 @@
+__author__ = 'aluno01'
+
+import math
+
+numFloat = 1.23545648561687
+
+numInt = 25
+
+#arredonda para cima
+print(math.ceil(numFloat))
+
+#arredonda para baixo
+print(math.floor(numFloat))
+
+#fatorial
+print(math.factorial(numInt))
+
+#retorna true caso seja letra e False caso seja numero
+print(math.isnan(numInt))
